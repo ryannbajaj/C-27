011023bb01d8610b34d37db71bda86fb7059adde
ryannbajaj <82589750+ryannbajaj@users.noreply.github.com>
2021-07-28 20:53:30 +0530

    Add files via upload

---
+++ pig.js
@@ -0,0 +1,26 @@
+class Pig {
+  constructor(x, y) {
+    var options = {
+        'restitution':0.8,
+        'friction':0.3,
+        'density':1.0
+    }
+    this.body = Bodies.circle(x, y, 35, options);
+    this.width = 35;
+    this.height = 35;
+    
+    World.add(world, this.body);
+  }
+  display(){
+    var pos =this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    ellipseMode(RADIUS);
+    fill("green");
+    ellipse(0, 0, this.width, this.height);
+    pop();
+    
+  }
+};
